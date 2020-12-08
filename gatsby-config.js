a623b0504ab63689631a4b6df1b13f10aa25d3f1
jb0nd87 <jbondoc87@gmail.com>
2020-12-08 11:10:08 -0800

    new gatsby portfolio files

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,6 +28,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/content/hero.md
@@ -0,0 +1,9 @@
+---
+greetings: "Hello"
+emoji: "👋"
+title: "I'm Jaime Bondoc"
+subtitlePrefix: "I build and design "
+subtitleHighlight: "things for the web."
+---
+
+Freelance Web Developer. Based in California.
