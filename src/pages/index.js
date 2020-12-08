5c3fd5b7199754dbc23c474dc89dc741658894d6
jb0nd87 <jbondoc87@gmail.com>
2020-12-08 11:19:21 -0800

    updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
-import Layout from '../components/layout'
-import Hero from '../components/hero'
+import { graphql } from "gatsby"
 
-const IndexPage = () => {
+import Layout from "../components/layout"
+import Hero from "../components/hero"
+
+const IndexPage = ({ data }) => {
   return (
     <Layout>
-      <Hero />
+      <Hero content={data.hero.edges[0].node} />
     </Layout>
   )
 }
